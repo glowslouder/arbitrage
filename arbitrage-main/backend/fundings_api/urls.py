bea591d7b5337e65e5a8f94c0a812f3845df6767
glowslouder <glows_louder.5l@icloud.com>
2025-07-18 01:48:02 +0300

    Delete arbitrage-main directory

--- arbitrage-main/backend/fundings_api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import getFundingsView
-
-urlpatterns = [
-    path('get/', getFundingsView)
-]
--- arbitrage-main/utils/middleware.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponseForbidden
-
-class CheckOriginMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-    def __call__(self, request, *args, **kwds):
-        origin = request.headers.get('Origin')
-        return self.get_response(request)
